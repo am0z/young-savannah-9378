d406c4a56b6a077b9c628735599aed7a1f3552c9
Andrey Mozgunov <am0z@ya.ru>
2016-05-21 06:01:51 +0300

    First selenium test.
    Signed-off-by: Andrey Mozgunov <am0z@ya.ru>

--- youngsters/tests.py
+++ youngsters/tests.py
@@ -1,5 +1,7 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class YoungTests(TestCase):
@@ -8,3 +10,25 @@ class YoungTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "hello")
         self.assertTemplateUsed(response, 'base.html')
+
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super(MySeleniumTests, cls).setUpClass()
+        cls.selenium = WebDriver()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("email")
+        username_input.send_keys('am0z')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('1q2w3e')
+        self.selenium.find_element_by_xpath('//button[contains(.,"Login")]').click()
