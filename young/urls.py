c4aa8f9e92c79b38fdc2b315c327ab03944afb99
Andrey Mozgunov <am0z@ya.ru>
2016-05-21 07:46:24 +0300

    Updates settings to use `django-environ` package. Added `brotlipy` as dependancy. Configured redis cache.
    Signed-off-by: Andrey Mozgunov <am0z@ya.ru>

--- young/urls.py
+++ young/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_page
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^$', cache_page(60 * 15)(
+        TemplateView.as_view(template_name='base.html')), name='home'),
     url(r'^', include('youngsters.urls', namespace='youngsters')),
     url(r'^', include('contact.urls', namespace='contact')),
 )
